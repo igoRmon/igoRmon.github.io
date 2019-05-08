953279ad03b3f1ceebbf508f19801d946b38d61d
Igor Ramon Fernandes Diniz <igor.ramon14@gmail.com>
2019-05-07 23:04:06 -0300

    Nova interface do portifolio

---
+++ style.js
@@ -0,0 +1,18 @@
+
+var a = document.createElement('a');
+var btn1 = document.querySelector('#btn-main');
+btn1.onclick = ()=>{
+    window.location.href = '#welcome-section';
+}
+
+var btn2 = document.querySelector('#btn-about');
+btn2.onclick = ()=>{
+    window.location.href = '#about';
+}
+
+var btn3 = document.querySelector('#btn-contact');
+btn3.onclick = ()=>{
+    window.location.href = '#contact';
+}
+
+
