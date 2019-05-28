e68020a93ea47433beffbbacb3756a13bc9a5381
Igor Ramon Fernandes Diniz <igor.ramon14@gmail.com>
2019-05-27 23:52:56 -0300

    Adicionando transição textual e alterando a scroll bar

--- style.js
+++ style.js
@@ -1,16 +1,34 @@
 var btn1 = document.querySelector('#btn-main');
-btn1.onclick = ()=>{
+btn1.onclick = () => {
     window.location.href = '#welcome-section';
 }
 
 var btn2 = document.querySelector('#btn-about');
-btn2.onclick = ()=>{
+btn2.onclick = () => {
     window.location.href = '#about';
 }
 
 var btn3 = document.querySelector('#btn-contact');
-btn3.onclick = ()=>{
+btn3.onclick = () => {
     window.location.href = '#contact';
 }
 
+function transition() {
+    let texto = document.querySelectorAll('.me');
+    texto.forEach(a => {
+        a.style.transition = "4s";
+        a.style.color = "white";
+    });
+}
+transition();
 
+window.onscroll = () => {
+    if (document.scrollingElement.scrollTop >= 549) {
+        let h2 = document.getElementById('title');
+        let p = document.getElementById('description');
+        h2.style.transition = "2s";
+        h2.style.color = "black";
+        p.style.transition = "4s";
+        p.style.color = "black";
+    }
+}
